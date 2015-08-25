c5b5bc9db5d191afe81bce52aff19bb7fe2bc785
Muhammad Gilang Januar <mgilangjanuar@gmail.com>
2015-08-25 08:20:14 +0700

    full delete book CRUD

--- app/views/admin/book/_form.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    use \helpers\Form;
-?>
-
-<?= $form = new Form; ?>
-<?= $form->inputText($model, 'title') ?>
-<?= $form->inputText($model, 'writer') ?>
-<?= $form->submitButton($model->isNewRecord() ? 'Create' : 'Update') ?>
